4f9edb4676c92d749d31ac03d0d35205fda1837c
night <wangleip7@163.com>
2021-12-28 19:45:57 +0800

    提交test1.html

--- go/src/Go-IM-System/client.go
+++ go/src/Go-IM-System/client.go
@@ -1,8 +1,11 @@
 package main
 
 import(
+    "flag"
     "net"
     "fmt"
+    "io"
+    "os"
 )
 
 type Client struct{
@@ -10,13 +13,15 @@ type Client struct{
     ServerPort int
     Name       string
     conn       net.Conn
+    flag       int //当前client模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client{
     //创建客户端对象
     client := &Client{
-        ServerIp: serverIp,
+        ServerIp:   serverIp,
         ServerPort: serverPort,
+        flag:       999,
     }
 
     //链接server
@@ -28,16 +33,97 @@ func NewClient(serverIp string, serverPort int) *Client{
 
     client.conn = conn
 
+    //返回对象
     return client
 }
 
+//处理server回应的消息,直接显示到标准输出即可
+func(client *Client) DealResponse(){
+
+    //一旦client.conn有数据,就直接copy到stdout标准输出上,永久阻塞监听
+    io.Copy(os.Stdout, client.conn)
+
+}
+
+func (client *Client) menu() bool {
+    var flag int
+
+    fmt.Println("1.公聊模式")
+    fmt.Println("2.私聊模式")
+    fmt.Println("3.更新用户名")
+    fmt.Println("0.退出")
+
+    fmt.Scanln(&flag)
+    if flag >= 0 && flag <= 3 {
+        client.flag = flag
+        return true
+    } else {
+        fmt.Println(">>>>>>请输入合法范围内的数字<<<<<<")
+        return false
+    }
+}
+
+
+func (client *Client) UpdateName() bool{
+    fmt.Println(">>>请输入用户名:")
+    fmt.Scanln(&client.Name)
+
+    sendMsg := "rename|" + client.Name + "\n"
+    _, err := client.conn.Write([]byte(sendMsg))
+    if err != nil{
+        fmt.Println("conn.write err:", err)
+        return false
+    }
+    return true
+}
+
+func (client *Client) Run(){
+    for client.flag !=0 {
+        for client.menu() != true{
+
+        }
+
+        //根据不同的模式处理不同的业务
+        switch client.flag{
+        case 1:
+            fmt.Println("公聊模式选择...")
+            break
+        case 2:
+            fmt.Println("私聊模式选择...")
+            break
+        case 3:
+            //更新用户名
+            client.UpdateName()
+            break
+        }
+    }
+}
+
+
+var serverIp string
+var serverPort int
+
+// ./client -ip 127.0.0.1
+func init(){
+    flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址(默认是127.0.0.1)")
+    flag.IntVar(&serverPort, "port", 8888, "设置服务器端口(默认是8888)")
+}
+
 func main(){
-    client := NewClient("127.0.0.1", 8888)
+    //命令行解析
+    flag.Parse()
+
+    client := NewClient(serverIp, serverPort)
     if client == nil{
         fmt.Println(">>>>>链接服务器失败...")
         return
     }
+
+    //单独开启一个goroutine去处理server的回执消息
+    go client.DealResponse()
+
     fmt.Println(">>>>>链接服务器成功...")
 
-    select{}
+    //启动客户端的业务
+    client.Run()
 }
